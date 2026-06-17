684ff132a14d7b5379427b0480fae310e15ee922
durgappagari.vaishnavi@zelis.com <durgappagari.vaishnavi@zelis.com>
2026-06-17 09:32:02 +0000

    added col

--- models/3.Inter/intr_orders_without_customers.sql
+++ models/3.Inter/intr_orders_without_customers.sql
@@ -6,7 +6,9 @@ select
     o.product_name,
     o.order_date,
     o.quantity,
-    o.amount
+    o.amount,
+    c.customer_name,
+    c.city
 from {{ ref('stg_orders') }} o
 left join {{ ref('stg_customers') }} c
     on o.customer_id = c.customer_id
